a819bdf7f98d9edf68a19e16170815f266934074
sts10 <sschlinkert@gmail.com>
2015-12-11 23:09:58 -0500

    day 5 passes 11 tests

--- day_5/runner.rb
+++ day_5/runner.rb
@@ -6,19 +6,28 @@ class Child
     @array = string.each_char.to_a
   end
   def nice?
-    self.three_vowels? && self.twice_row && !self.has_a_bad_combo
+    self.three_vowels? && self.twice_row? && !self.has_a_bad_combo?
   end
 
   def three_vowels?
-    vowels = ['a','e','i','o','u']
-    count = @array.count { |l| vowels.include?(l) }
-    # if count >= 3 ? true : false
-    count >=3
+    @array.count { |l| ['a','e','i','o','u'].include?(l) } >= 3
   end
 
+  def twice_row?
+    @array.map.with_index { |l, i| l == @array[i+1] }.any?
+    # @array.any? do |l|
+    #   next_l = @array[@array.index(l)+1]
+    #   next_l && l == next_l
+    # end
+  end
+
+  def has_a_bad_combo?
+    combo_array = @array.map.with_index { |l, i| l + @array[i+1] if @array[i+1] }
+    combo_array.any? { |combo| ["ab","cd","pq","xy"].include?(combo.to_s) }
+  end
 end
 
-
+Child.new("abbcdd").twice_row?
 # file_string = ""
 # File.open(file_location, "r") do |f|
 #   f.each_line do |line|
--- day_5/spec/day_3_spec.rb
+++ day_5/spec/day_3_spec.rb
@@ -1,7 +1,7 @@
 require_relative './spec_helper.rb'
 require_relative '../runner.rb'
 
-describe 'Naughty or nice?' do 
+describe 'Naughty or nice components' do 
   it 'knows if a Child has three vowels' do
     @this_child = Child.new("kdebidddu")
 
@@ -11,5 +11,53 @@ describe 'Naughty or nice?' do
     @this_child = Child.new("kkslkalefff")
 
     expect(@this_child.three_vowels?).to be(false)
+  end
+
+  it 'know if a Child has two characters in a row' do
+    @this_child = Child.new("ihjdfbb")
+
+    expect(@this_child.twice_row?).to be(true)
+  end
+  it 'know if a Child does not have two characters in a row' do
+    @this_child = Child.new("ihjdfb")
+
+    expect(@this_child.twice_row?).to be(false)
+  end
+
+  it 'knows if a child has a bad combo' do
+    @this_child = Child.new("tkkabdd")
+
+    expect(@this_child.has_a_bad_combo?).to be(true)
+  end
+  it 'knows if child does not have a bad combo' do
+    @this_child = Child.new("ihjdfb")
+
+    expect(@this_child.has_a_bad_combo?).to be(false)
+  end
+end
+
+describe 'Full naughty or nice question' do
+  it 'knows if a child is nice' do
+    @this_child = Child.new("ugknbfddgicrmopn")
+    expect(@this_child.nice?).to be(true)
+  end
+
+  it 'knows aaa is nice' do
+    @this_child = Child.new("aaa")
+    expect(@this_child.nice?).to be(true)
+  end
+
+  it 'knows jchzalrnumimnmhp is naughty' do
+    @this_child = Child.new("jchzalrnumimnmhp")
+    expect(@this_child.nice?).to be(false)
+  end
+
+  it 'knows haegwjzuvuyypxyu is naughty' do 
+    @this_child = Child.new("haegwjzuvuyypxyu")
+    expect(@this_child.nice?).to be(false)
+  end
+  it 'knows dvszwmarrgswjxmb is naughty' do 
+    @this_child = Child.new("dvszwmarrgswjxmb")
+    expect(@this_child.nice?).to be(false)
   end
 end
